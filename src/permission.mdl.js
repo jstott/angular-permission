6be0dbf179d9edfb9b590c71d0026b95d9d29766
Jim Stott <jwstott@gmail.com>
2014-09-14 11:58:27 -0400

    cleanup - organize $stateChangeStart event handler
    If authorized - do nothing
    On authorized check - pass name of role being tested to definedRole function

--- src/permission.mdl.js
+++ src/permission.mdl.js
@@ -1,48 +1,32 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('permission', ['ui.router'])
-    .run(function ($rootScope, Permission, $state) {
-      $rootScope.$on('$stateChangeStart',
-      function (event, toState, toParams, fromState, fromParams) {
-        // If there are permissions set then prevent default and attempt to authorize
-        var permissions;
-        if (toState.data && toState.data.permissions) {
-          permissions = toState.data.permissions;
-        } else if (toState.permissions) {
-          /**
-          * This way of defining permissions will be depracated in v1. Should use
-          * `data` key instead
-          */
-          console.log('Deprecation Warning: permissions should be set inside the `data` key ');
-          console.log('Setting permissions for a state outside `data` will be depracated in' +
-            ' version 1');
-          permissions = toState.permissions;
-        }
-
-        if (permissions) {
-          event.preventDefault();
-
-          Permission.authorize(permissions).then(function () {
-            // If authorized, use call state.go without triggering the event.
-            // Then trigger $stateChangeSuccess manually to resume the rest of the process
-            // Note: This is a pseudo-hacky fix which should be fixed in future ui-router versions
-            $state.go(toState.name, toParams, {notify: false}).then(function() {
-              $rootScope
-                .$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
-            });
-
-          }, function () {
-            // If not authorized, redirect to wherever the route has defined, if defined at all
-            var redirectTo = permissions.redirectTo;
-            if (redirectTo) {
-              $state.go(redirectTo, {}, {notify: false}).then(function() {
-                $rootScope
-                  .$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
-              });
+    var app = angular.module('permission', ['ui.router']);
+    app.run(['$rootScope', 'permission', '$state', function ($rootScope, Permission, $state) {
+        $rootScope.$on('$stateChangeStart',
+        function (event, toState, toParams, fromState, fromParams) {
+            // If there are permissions set then prevent default and attempt to authorize
+            if (toState.data && toState.data.permissions) {
+                  Permission.authorize(toState.data.permissions)
+                      .then(authorized)
+                      .catch(rejected);
             }
-          });
-        }
-      });
-    });
+
+            function authorized() {
+              // really nothing to do - let the navigation continue
+              $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
+            }
+            
+            function rejected() {
+                var redirectTo = permissions.redirectTo;
+                event.preventDefault();
+                if (redirectTo) {
+                    //console.log('redirectTo: ' + redirectTo);
+                    $state.go(redirectTo);
+                    $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
+                }
+            }
+        });
+
+    }]);
 }());
